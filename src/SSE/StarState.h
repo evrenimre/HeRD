fc0c7de6c2dda3ec2c75e6c7823e52b07ebd7698
Evren <evren.imre@gmail.com>
2021-05-03 14:25:56 +0100

    Fix typo

--- src/SSE/StarState.h
+++ src/SSE/StarState.h
@@ -24,7 +24,7 @@ struct StarState
   Herd::Generic::Mass m_Mass;	///< Mass in \f$ M_{\odot}\f$
   Herd::Generic::Metallicity m_Z; ///< Metallicity
   Herd::Generic::Radius m_Radius;	///< Radius in \f$ R_{\odot}\f$
-  Herd::Generic::Luminosity m_Lumonosity;	///< Luminosity in \f$ L_{\odot}\f$
+  Herd::Generic::Luminosity m_Luminosity;	///< Luminosity in \f$ L_{\odot}\f$
   Herd::Generic::Temperature m_Temperature;	///< Temperature in K
   Herd::Generic::Age m_Age;	///< Age in million years
 };
