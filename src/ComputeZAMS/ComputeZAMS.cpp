297bccc7459b157b85e9e416df408e28d3df38b6
Evren <evren.imre@gmail.com>
2021-05-06 23:13:37 +0100

    Command line tool to compute the ZAMS state

---
+++ src/ComputeZAMS/ComputeZAMS.cpp
@@ -0,0 +1,70 @@
+/**
+ * @file ComputeZAMS.cpp
+ * @author Evren Imre
+ * @date 6 May 2021	
+ */
+/* This file is a part of HeRD, a stellar evolution library
+ * Copyright © 2021 Evren Imre 
+ *
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at https://mozilla.org/MPL/2.0/. */
+
+#include <Generic/Quantities.h>
+#include <SSE/StarState.h>
+#include <SSE/ZeroAgeMainSequence.h>
+
+#include <iostream>
+#include <optional>
+
+#include <boost/program_options.hpp>
+
+/**
+ * @brief Computes the ZAMS luminosity, radius and temperature
+ * @param argc Argument count
+ * @param argv Arguments
+ * @return Status code
+ */
+int main( int argc, char* argv[] )
+{
+  boost::program_options::options_description description( "Arguments" );
+
+  // @formatter:off
+  description.add_options()( "help,", "Display this message" )
+      ( "mass,", boost::program_options::value< double >(), "Mass in M_Sun" )
+      ( "metallicity", boost::program_options::value< double >(), "Metallicity" );
+    // @formatter:on
+
+  boost::program_options::variables_map argument_map;
+  boost::program_options::store( boost::program_options::command_line_parser( argc, argv ).options( description ).run(), argument_map );
+  boost::program_options::notify( argument_map );
+
+  if( argument_map.count( "help" ) == 1 )
+  {
+    std::cout << description;
+  }
+
+  if( argument_map.count( "mass" ) == 1 && argument_map.count( "metallicity" ) == 1 )
+  {
+    Herd::Generic::Mass mass( argument_map[ "mass" ].as< double >() );
+    Herd::Generic::Metallicity metallicity( argument_map[ "metallicity" ].as< double >() );
+
+    std::optional< Herd::SSE::StarState > zamsState = Herd::SSE::ZeroAgeMainSequence::ComputeStarState( mass, metallicity );
+
+    if( zamsState )
+    {
+      std::cout << " Luminosity " << zamsState->m_Luminosity.Value() << " L_Sun \n";
+      std::cout << " Radius " << zamsState->m_Radius.Value() << " R_Sun \n";
+      std::cout << " Temperature " << zamsState->m_Temperature.Value() << " K \n";
+    } else
+    {
+      std::cout << "Invalid arguments \n";
+      return 1;
+    }
+  }
+
+  return 0;
+}
+
+
+
