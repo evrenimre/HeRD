191be0c9ef9cefdbfdc8ce37eefc85792b6c4eec
Evren <evren.imre@gmail.com>
2021-05-27 21:37:06 +0100

    Rename modules

--- cmake/HerdCreateLibrary.cmake
+++ cmake/HerdCreateLibrary.cmake
@@ -25,3 +25,28 @@ macro( add_interface_library_common )
 
 	target_link_libraries( ${TARGET_NAME} INTERFACE ${COVERAGE_CONFIG} )	# Instruments the library and its dependents for coverage analysis 
 endmacro()
+
+# Common code for adding an install target
+# TARGET_NAME and HEADER_LIST defined by caller
+
+macro( add_install_target_common )
+	
+	# Installation
+	set( EXPORT_TARGET_NAME "${TARGET_NAME}Targets" )
+	install( TARGETS ${TARGET_NAME} EXPORT ${EXPORT_TARGET_NAME}
+										       LIBRARY DESTINATION "${INSTALL_LIB_DIR}"
+										       ARCHIVE DESTINATION "${INSTALL_LIB_DIR}"
+										       RUNTIME DESTINATION "${INSTALL_BIN_DIR}"
+										       INCLUDES DESTINATION "${INSTALL_INCLUDE_DIR}/${TARGET_NAME}"
+	)
+	
+	install( FILES ${HEADER_LIST} DESTINATION "${INSTALL_INCLUDE_DIR}/${TARGET_NAME}")
+	
+	
+	install( EXPORT ${EXPORT_TARGET_NAME}
+	         FILE ${EXPORT_TARGET_NAME}.cmake
+	         NAMESPACE ${CMAKE_PROJECT_NAME}::
+	         DESTINATION "${INSTALL_LIB_DIR}/cmake/${TARGET_NAME}"
+	)
+
+endmacro()
