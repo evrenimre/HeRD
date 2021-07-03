9e0a50f6e1049afee25872eb4e2800969494103c
Evren <evren.imre@gmail.com>
2021-07-03 18:54:07 +0100

    Clang-tidy binary name can now be specified by CLANG_TIDY_NAME

--- cmake/HerdConfigureStaticCheckers.cmake
+++ cmake/HerdConfigureStaticCheckers.cmake
@@ -4,9 +4,13 @@ if(NOT DEFINED CONFIG_DIR)
 	message(FATAL_ERROR "CONFIG_DIR not defined")
 endif()
 
+if(NOT DEFINED CLANG_TIDY_NAME)
+	message(FATAL_ERROR "CLANG_TIDY_NAME not defined")
+endif()
+
 set(CMAKE_LINK_WHAT_YOU_USE ON)
 
-find_program(CLANG_TIDY clang-tidy)
+find_program(CLANG_TIDY ${CLANG_TIDY_NAME})
 if(CLANG_TIDY)
 	set(CMAKE_CXX_CLANG_TIDY ${CLANG_TIDY}; -checks=*,-modernize-use-trailing-return-type,-readability-named-parameter # All tests except
 																			    -line-filter=[{"name":".*\.UnitTests.cpp"}]	# Exclude unit tests
