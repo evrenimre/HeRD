0373f881f251614cc13681aefd96ae08e886b179
Evren <evren.imre@gmail.com>
2021-07-18 12:01:29 +0100

    Use gold if available

--- cmake/HerdFlagsGNU.cmake
+++ cmake/HerdFlagsGNU.cmake
@@ -8,4 +8,9 @@ set(LINKER_CXX_COVERAGE_FLAGS --coverage)
 set(COMPILER_CXX_SANITISER_FLAGS -O0 -g -fno-omit-frame-pointer -fsanitize=address -fsanitize=undefined)	# Sanitiser
 set(LINKER_CXX_SANITISER_FLAGS -fsanitize=address -fsanitize=undefined)	# Sanitiser
 
+find_program(GOLD gold)
+if(GOLD)
+	set(LINKER_CXX_LD_FLAGS -fuse-ld=gold)
+	message(STATUS "Using ${GOLD}")
+endif()
 # g++ links libstdc++ and libm by default. No need to explicitly link against them 
