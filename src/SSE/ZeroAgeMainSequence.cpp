4b3a05a2801a3d5ab50a47ede80ce989b92e0c0a
Evren <evren.imre@gmail.com>
2021-08-25 10:56:12 +0100

    Refactoring and simplification

--- src/SSE/ZeroAgeMainSequence.cpp
+++ src/SSE/ZeroAgeMainSequence.cpp
@@ -13,7 +13,7 @@
 #include "ZeroAgeMainSequence.h"
 
 #include "Constants.h"
-#include "SSEHelpers.h"
+#include "MathHelpers.h"
 
 #include <Generic/Quantity.h>
 #include <Generic/QuantityRange.h>
@@ -22,8 +22,6 @@
 #include <algorithm>
 #include <cmath>
 #include <cstddef>
-
-#include <boost/math/special_functions/pow.hpp>
 
 namespace
 {
@@ -35,7 +33,7 @@ const std::array<double, 35 > s_ZL { 3.970417e-01, -3.2913574e-01, 3.4776688e-01
     5.563579e+00, -1.032345224e+01, 1.944322980e+01, 1.897361347e+01, 4.16903097e+00,
     7.8866060e-01, -2.90870942e+00, 6.54713531e+00, 4.05606657e+00, 5.3287322e-01,
     5.86685e-03, -1.704237e-02, 3.872348e-02, 2.570041e-02, 3.83376e-03
-};  ///< Coefficients for luminosity calculation
+};  ///< Coefficients for \f$ L_{ZAMS}(z) \f$
 
 const std::array< double, 45 > s_ZR { 1.715359e+00, 6.2246212e-01, -9.2557761e-01, -1.16996966e+00, -3.0631491e-01,
     6.597788e+00, -4.2450044e-01, -1.213339427e+01, -1.073509484e+01, -2.51487077e+00,
@@ -46,7 +44,7 @@ const std::array< double, 45 > s_ZR { 1.715359e+00, 6.2246212e-01, -9.2557761e-0
     3.082234e+00, 9.447205e-01, -2.15200882e+00, -2.49219496e+00, -6.3848738e-01,
     1.784778e+01, -7.4534569e+00, -4.896066856e+01, -4.005386135e+01, -9.09331816e+00,
     2.2582e-04, -1.86899e-03, 3.88783e-03, 1.42402e-03, -7.671e-05
-};  ///< Coefficients for radius calculation
+};  ///< Coefficients for \f$ R_{ZAMS}(z) \f$
 // @formatter:on
 
 }
@@ -106,23 +104,26 @@ void ZeroAgeMainSequence::Validate( Herd::Generic::Mass i_Mass, Herd::Generic::M
  */
 Herd::Generic::Luminosity ZeroAgeMainSequence::ComputeLuminosity( Herd::Generic::Mass i_Mass, const std::array< double, 5 >& i_rZPowers )
 {
-  std::array< double, 7 > eq1Coeffs;
-  Herd::SSE::MultiplyMatrixVector( eq1Coeffs, s_ZL, i_rZPowers );  //Eq3
+  std::array< double, 7 > a;  // Coefficients
+  Herd::SSE::MultiplyMatrixVector( a, s_ZL, i_rZPowers );  //Eq. 3
 
   // Powers of mass
+  std::array< double, 6 > massPowers;
+
   double m05 = std::sqrt( i_Mass );
   double m20 = i_Mass * i_Mass;
-  double m30 = m20 * i_Mass;
-  double m50 = m30 * m20;
-  double m55 = m50 * m05;
-  double m70 = m50 * m20;
-  double m80 = m50 * m30;
-  double m95 = m55 * m20 * m20;
-  double m110 = m55 * m55;
+  massPowers[ 0 ] = 1;
+  massPowers[ 1 ] = m20 * i_Mass;  // m^3
+  massPowers[ 2 ] = massPowers[ 1 ] * m20;  // m^5
+  massPowers[ 3 ] = massPowers[ 2 ] * m20;  // m^7
+  massPowers[ 4 ] = massPowers[ 3 ] * i_Mass; // m^8
+  massPowers[ 5 ] = massPowers[ 4 ] * i_Mass * m05; // m^9.5
 
-  // Eq1
-  double num = eq1Coeffs[ 0 ] * m55 + eq1Coeffs[ 1 ] * m110;
-  double den = eq1Coeffs[ 2 ] + m30 + eq1Coeffs[ 3 ] * m50 + eq1Coeffs[ 4 ] * m70 + eq1Coeffs[ 5 ] * m80 + eq1Coeffs[ 6 ] * m95;
+  double m55 = massPowers[ 2 ] * m05; // m5.5
+
+  // Eq. 1
+  double num = m55 * ( a[ 0 ] + m55 * a[ 1 ] );
+  double den = Herd::SSE::ComputeInnerProduct( { a[ 2 ], 1., a[ 3 ], a[ 4 ], a[ 5 ], a[ 6 ] }, massPowers );
 
   return Herd::Generic::Luminosity( num / den );
 }
@@ -134,23 +135,32 @@ Herd::Generic::Luminosity ZeroAgeMainSequence::ComputeLuminosity( Herd::Generic:
  */
 Herd::Generic::Radius ZeroAgeMainSequence::ComputeRadius( Herd::Generic::Mass i_Mass, const std::array< double, 5 >& i_rZPowers )
 {
-  std::array< double, 9 > eq2Coeffs;
-  Herd::SSE::MultiplyMatrixVector( eq2Coeffs, s_ZR, i_rZPowers );  //Eq3
+  std::array< double, 9 > a;
+  Herd::SSE::MultiplyMatrixVector( a, s_ZR, i_rZPowers );  //Eq. 3
 
   // Powers of mass
   double m05 = std::sqrt( i_Mass );
   double m20 = i_Mass * i_Mass;
-  double m25 = m20 * m05;
-  double m65 = boost::math::pow< 3 >( m20 ) * m05;
-  double m85 = m65 * m20;
-  double m110 = m85 * m25;
-  double m185 = m110 * m65 * i_Mass;
-  double m190 = m185 * m05;
-  double m195 = m110 * m85;
+  double m30 = m20 * i_Mass;
+  double m50 = m30 * m20;
 
-  // Eq2
-  double num = eq2Coeffs[ 0 ] * m25 + eq2Coeffs[ 1 ] * m65 + eq2Coeffs[ 2 ] * m110 + eq2Coeffs[ 3 ] * m190 + eq2Coeffs[ 4 ] * m195;
-  double den = eq2Coeffs[ 5 ] + eq2Coeffs[ 6 ] * m20 + eq2Coeffs[ 7 ] * m85 + m185 + eq2Coeffs[ 8 ] * m195;
+  std::array< double, 5 > massPowersNum;
+  massPowersNum[ 0 ] = m20 * m05; // m^2.5
+  massPowersNum[ 1 ] = m30 * m30 * m05; // m^6.5
+  massPowersNum[ 2 ] = m30 * m30 * m50; // m^11
+  massPowersNum[ 3 ] = massPowersNum[ 1 ] * massPowersNum[ 1 ] * m50 * i_Mass; // m^19
+  massPowersNum[ 4 ] = massPowersNum[ 3 ] * m05;  // m^19.5
+
+  std::array< double, 5 > massPowersDen;
+  massPowersDen[ 0 ] = 1;
+  massPowersDen[ 1 ] = m20;
+  massPowersDen[ 2 ] = massPowersNum[ 1 ] * m20;  // m^8.5
+  massPowersDen[ 3 ] = massPowersNum[ 2 ] * massPowersNum[ 1 ] * i_Mass; // m^18.5
+  massPowersDen[ 4 ] = massPowersNum[ 4 ];  // m^19.5
+
+  // Eq. 2
+  double num = Herd::SSE::ComputeInnerProduct( { a[ 0 ], a[ 1 ], a[ 2 ], a[ 3 ], a[ 4 ] }, massPowersNum );
+  double den = Herd::SSE::ComputeInnerProduct( { a[ 5 ], a[ 6 ], a[ 7 ], 1., a[ 8 ] }, massPowersDen );
 
   return Herd::Generic::Radius( num / den );
 }
