6fcfeb66564f1f787dbef9b760afea964a62b113
Evren <evren.imre@gmail.com>
2022-12-13 21:48:00 +0000

    Clang does not recognise the flag -static-libasan

--- cmake/HerdFlagsClang.cmake
+++ cmake/HerdFlagsClang.cmake
@@ -1,2 +1,7 @@
 # This works as long as the flags used in the GNU module are identical with their Clang equivalents
 include(HerdFlagsGNU)
+
+# Sanitiser
+# clang does not recognise the flag -static-libasan used by g++ for statically linking ASan
+set(LINKER_CXX_SANITISER_FLAGS -fsanitize=address -fsanitize=undefined)
+
