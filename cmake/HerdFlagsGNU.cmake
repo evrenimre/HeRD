d48442e3a6cc05d9d4d585a90c146c017793deee
Evren <evren.imre@gmail.com>
2022-12-18 16:37:42 +0000

    Fixed user unable to set the build type. Robust handling of unset build type.

--- cmake/HerdFlagsGNU.cmake
+++ cmake/HerdFlagsGNU.cmake
@@ -2,10 +2,10 @@
 
 set(COMPILER_CXX_WARNING_FLAGS "-Wall -Wextra --pedantic -Werror" )	# Enables all warnings for more diagnostics
 
-set(COMPILER_CXX_COVERAGE_FLAGS -O0 -g --coverage)	# Code coverage
+set(COMPILER_CXX_COVERAGE_FLAGS --coverage)	# Code coverage
 set(LINKER_CXX_COVERAGE_FLAGS --coverage)
 
-set(COMPILER_CXX_SANITISER_FLAGS -O0 -g -fno-omit-frame-pointer -fsanitize=address -fsanitize=undefined)	# Sanitiser
+set(COMPILER_CXX_SANITISER_FLAGS -fno-omit-frame-pointer -fsanitize=address -fsanitize=undefined)	# Sanitiser
 set(LINKER_CXX_SANITISER_FLAGS -fsanitize=address -fsanitize=undefined -static-libasan)	# Sanitiser
 
 #Linker
